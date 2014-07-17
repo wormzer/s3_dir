f93b02ef9bd4f1243bcadf59ce0de27fd981ff0c
Jeff Byrnes <jeff@evertrue.com>
2014-07-17 17:18:45 -0400

    Align resource attributes for readability

--- resources/default.rb
+++ resources/default.rb
@@ -1,11 +1,11 @@
 actions :create
 
-attribute :name, kind_of: String, name_attribute: true
-attribute :owner, kind_of: [String, NilClass], default: nil
-attribute :group, kind_of: [String, NilClass], default: nil
-attribute :mode, kind_of: [String, Integer, NilClass], default: nil
-attribute :bucket, kind_of: String
-attribute :dir, kind_of: String, default: '/'
-attribute :recursive, kind_of: [TrueClass, FalseClass], default: false
-attribute :access_key_id, kind_of: String
+attribute :name,              kind_of: String, name_attribute: true
+attribute :owner,             kind_of: [String, NilClass],          default: nil
+attribute :group,             kind_of: [String, NilClass],          default: nil
+attribute :mode,              kind_of: [String, Integer, NilClass], default: nil
+attribute :bucket,            kind_of: String
+attribute :dir,               kind_of: String,                      default: '/'
+attribute :recursive,         kind_of: [TrueClass, FalseClass],     default: false
+attribute :access_key_id,     kind_of: String
 attribute :secret_access_key, kind_of: String
