6b47eefcd9c72972293ec4e074e002d251d9ec3b
Jeff Byrnes <jeff@evertrue.com>
2014-07-17 18:06:27 -0400

    Add ChefSpec test (yep, just one)

---
+++ spec/recipes/default_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 's3_dir::default' do
+  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+  before do
+    Chef::EncryptedDataBagItem.stub(:load).with('secrets', 'aws_credentials').and_return(
+      'RailsDeploy-dev' => {
+        'access_key_id'     => 'SAMPLE_ACCESS_KEY_ID',
+        'secret_access_key' => 'SECRET_ACCESS_KEY'
+      }
+    )
+  end
+
+  it 'downloads `/tmp/provisioning` from S3' do
+    expect(chef_run).to create_s3_dir('/tmp/provisioning').with(
+      bucket:            'provisioning.evertrue.com',
+      dir:               '/s3_dir_test',
+      access_key_id:     'SAMPLE_ACCESS_KEY_ID',
+      secret_access_key: 'SECRET_ACCESS_KEY'
+    )
+  end
+end
