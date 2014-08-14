2a1cc42366e21ca4accac7d47d895d17073c8196
Eric Herot <eric.github@herot.com>
2014-08-14 16:41:10 -0400

    Fix the unit tests to match the code

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -3,26 +3,16 @@ require 'spec_helper'
 describe 's3_dir::default' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
-  before do
-    allow(Chef::EncryptedDataBagItem).to receive(:load).with('secrets', 'aws_credentials')
-      .and_return(
-        'RailsDeploy-dev' => {
-          'access_key_id'     => 'SAMPLE_ACCESS_KEY_ID',
-          'secret_access_key' => 'SECRET_ACCESS_KEY'
-        }
-      )
-  end
-
   it 'includes the `et_fog::default` recipe' do
     expect(chef_run).to include_recipe 'et_fog::default'
   end
 
   it 'downloads `/tmp/provisioning` from S3' do
     expect(chef_run).to create_s3_dir('/tmp/provisioning').with(
-      bucket:            'provisioning.evertrue.com',
+      bucket:            'test-directory',
       dir:               '/s3_dir_test',
-      access_key_id:     'SAMPLE_ACCESS_KEY_ID',
-      secret_access_key: 'SECRET_ACCESS_KEY'
+      access_key_id:     'TEST_KEY',
+      secret_access_key: 'TEST_SECRET'
     )
   end
 end
