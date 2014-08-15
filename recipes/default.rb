b7c7b8529c6ec443b6a8d16349dd85634fb17c2f
Eric Herot <eric.github@herot.com>
2014-08-15 11:30:34 -0400

    Force apt-get update

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,11 @@
 # your wrapper cookbook.
 #
 
+execute 'apt-get update' do
+  command 'apt-get update'
+  action  :nothing
+end.run_action(:run)
+
 include_recipe 'et_fog::default'
 
 s3_dir '/tmp/provisioning' do
