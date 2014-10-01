bd0aa8672bbd8cd018a9e840a0206dcb8e92d61c
Eric Herot <eric.github@herot.com>
2014-10-01 16:16:48 -0400

    Bump s3_file to 2.5.0

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures s3_dir'
 long_description 'Installs/Configures s3_dir'
 version          '1.3.0'
 
-depends 's3_file', '>= 2.3.0'
+depends 's3_file', '>= 2.5.0'
 depends 'et_fog',  '~> 1.0'
