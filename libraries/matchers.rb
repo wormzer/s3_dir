0c3029d575ed70739f50e4d02b4f9e97cfb26e55
Jeff Byrnes <jeff@evertrue.com>
2014-10-21 11:57:35 -0400

    Replace ChefSpec::Runner.define_runner_method w/ ChefSpec.define_matcher
    As per ChefSpec deprecations warnings

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,5 +1,5 @@
 if defined?(ChefSpec)
-  ChefSpec::Runner.define_runner_method :s3_dir
+  ChefSpec.define_matcher :s3_dir
 
   def create_s3_dir(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:s3_dir, :create, resource_name)
